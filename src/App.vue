dd560889447c52ec99151ef8cb988f50f3a3e6d6
Subin Siby <mail@subinsb.com>
2020-06-30 16:19:08 +0530

    Keep components alive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <transition name='slide'>
-      <router-view/>
+      <keep-alive>
+        <router-view/>
+      </keep-alive>
     </transition>
   </div>
 </template>
