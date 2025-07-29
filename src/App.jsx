cacaf458031fc8b81ddb196aa47275e925856f6d
Subin Siby <mail@subinsb.com>
2025-07-29 23:51:18 +0530

    Setup header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 
-import { Header } from './components/Header'
+import Header from './components/Header'
+
+import { BrowserRouter, Routes, Route } from "react-router";
 
 import './style.css'
+import Home from './pages/Home';
+import Settings from './pages/Settings';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" component={Home} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" component={Home} />
+          <Route path="/settings" component={Settings} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="home">
+    <div>
 
     </div>
   )
