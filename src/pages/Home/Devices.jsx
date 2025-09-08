2fe97cca7986675f664b91b21ea8e3e1f4b6f276
Subin Siby <mail@subinsb.com>
2025-09-09 01:56:55 +0530

    Show devices in tags

--- src/pages/Home/Devices.jsx
+++ src/pages/Home/Devices.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
-import { useMainStore } from '../../store'
+import { useMainStore, usePersistentStore } from '../../store'
 
 const Devices = () => {
-  const { users } = useMainStore()
-  console.log(users)
+  const { users, p2pt } = useMainStore()
+  const { name } = usePersistentStore()
+
+  const ping = (id) => {
+    const user = users[id]
+    if (user) {
+      p2pt.send(user.conn, { type: 'ping' })
+    }
+  }
 
   return (
     <div>
-      {Object.values(users).map((user) => (
-        <div key={user.id}>{user.name}</div>
-      ))}
+      <div>
+        <span className="tag tag-grouped">
+          <span className="tag tag-gray">{name}</span>
+          <span className="tag tag-yellow">Me</span>
+        </span>
+      </div>
+      <div className="my-4" style={{ position: 'relative', borderBottom: '2px dashed rgb(219, 219, 219)' }}>
+        <span style={{ position: 'absolute', bottom: '-11px' }}>Devices</span>
+      </div>
+      <div className="flex flex-col">
+        {Object.values(users).map((user) => (
+          <div className="tag tag-grouped" key={user.id}>
+            <span className="tag tag-gray">{user.name}</span>
+            <span className="tag tag-yellow">
+              <a onClick={() => ping(user.id)}>Ping!</a>
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
